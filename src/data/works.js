87fb004aeebbf239174aaff60afe352c67b66b34
Jasjeet00 <71466695+Jasjeet00@users.noreply.github.com>
2022-11-28 11:57:13 -0330

    Update works.js

--- src/data/works.js
+++ src/data/works.js
@@ -1,9 +1,8 @@
 export default [
 	{
 		title: 'Resume',
-		imgUrl:
-			'<img width="136" alt="resume_screenshot" src="https://user-images.githubusercontent.com/71466695/204314407-71230d6b-4944-454d-8f05-a5f16fa8c6f2.png">
-',
+		imgUrl:'<https://raw.githubusercontent.com/Jasjeet00/Portfolio-updated/main/resume_screenshot.png">',
+			
 		tech: ['Resume'],
 		workUrl: 'https://drive.google.com/file/d/1DlEgxzqDFwYXNGN36xkDGx0VDZUBFleY/view?usp=sharing',
 	},
