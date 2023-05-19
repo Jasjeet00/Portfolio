c0cb2d03f0fa0f084766acf33bbc98628c804f1a
Jasjeet00 <71466695+Jasjeet00@users.noreply.github.com>
2023-05-19 12:50:41 -0230

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,8 +8,8 @@ function About() {
 			<div className="w-full md:w-6/12">
 				<SectionTitle>About Me</SectionTitle>
 				<p className="text-md text-gray-600 dark:text-gray-300">
-				I am passionate about front-end development and creating visually intriguing web designs, using my problem solving skills to come to
-				unique solutions and learning new languages and software. I am currently working on a fitness website called fitness first and learning about
+				I am passionate about front-end development, application development and creating visually intriguing web designs and mobile applications, using my problem solving skills to come to
+				unique solutions and learning new languages and software. I am currently working on building an mobile application that helps facilitate feedback for learners and educators in the radiology curriculum with Faculty of Medicine at Memorial University of Newfoundland and Labrador and learning about
 				React Native. When I am not programming you can find me working out or listening to some good tunes.
 				
 				</p>
